fdecc0f0ab1f8c3b311ef7bf59c6d2dbbe0f12cc
James Leong <jleong963@gmail.com>
2025-04-20 19:35:44 +0800

    add yml file for auto gen jar file & populate it to README.md for download

--- src/test/java/com/ApplicationTests.java
+++ src/test/java/com/ApplicationTests.java
@@ -1,24 +1,10 @@
 package com;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-/*HTTP test*/
-@AutoConfigureMockMvc
-/*Test DB*/
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 class ApplicationTests {
-
-	@Autowired
-	MockMvc mockMvc;
 
 	/*Disable test*/
 	//@Disabled("Temporarily disabled until bug XYZ is fixed")
