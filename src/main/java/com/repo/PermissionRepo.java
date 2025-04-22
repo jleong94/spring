92c085d8f886228983bb891b24506d7f1358380f
James Leong <james.leong@mpsb.net>
2025-04-22 13:06:41 +0800

    commit

--- src/main/java/com/repo/PermissionRepo.java
+++ src/main/java/com/repo/PermissionRepo.java
@@ -3,7 +3,9 @@ package com.repo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.modal.Permission;
+
 @Repository
-public interface PermissionRepo extends CrudRepository<PermissionRepo, Long> {
+public interface PermissionRepo extends CrudRepository<Permission, Long> {
 
 }
