630196251ef46a6baea1ae12efa2b9205abf2c87
James Leong <james.leong@mpsb.net>
2025-05-08 09:32:50 +0800

    code cleanup & add api call template in async way

--- src/main/java/com/api/JavaAPICall.java
+++ src/main/java/com/api/JavaAPICall.java
@@ -5,16 +5,21 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.util.concurrent.CompletableFuture;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 
@@ -38,7 +43,7 @@ public class JavaAPICall {
 		try {
 			log.info("URL: " + URL);
 			log.info("Request: " + objectMapper.writeValueAsString(object));
-			if(!URL.isEmpty()){
+			if(URL != null && !URL.isBlank()){
 				/*List<NameValuePair> params = new ArrayList<>();
 				params.add(new BasicNameValuePair("", ));*/
 				RequestConfig requestConfig = RequestConfig.custom()
@@ -129,12 +134,152 @@ public class JavaAPICall {
 		}
 		return result;
 	}
+
+	public String httpClientApiAsync(Logger log, String logFolder) {
+		String result = "";
+		String URL = "";
+		Object object = new Object();
+		CompletableFuture<Object> futureObject = new CompletableFuture<>();
+		ObjectMapper objectMapper = new ObjectMapper()
+				.registerModule(new JavaTimeModule());
+		try {
+			log.info("URL: " + URL);
+			log.info("Request: " + objectMapper.writeValueAsString(object));
+			if(URL != null && !URL.isBlank()){
+				/*List<NameValuePair> params = new ArrayList<>();
+				params.add(new BasicNameValuePair("", ));*/
+				RequestConfig requestConfig = RequestConfig.custom()
+		                .setConnectTimeout(5 * 1000)//in miliseconds
+		                .setSocketTimeout(5 * 1000)//in miliseconds
+		                .setConnectionRequestTimeout(5 * 1000)//in miliseconds
+		                .build();
+				@Cleanup CloseableHttpAsyncClient httpClient = HttpAsyncClients.custom()
+		                .setDefaultRequestConfig(requestConfig)
+		                .build();
+                httpClient.start();
+				HttpPost httpRequest = new HttpPost(URL);
+				/*HttpGet httpRequest = new HttpGet(URL);
+				URI uri = new URIBuilder(httpRequest.getURI())
+						.addParameters(params)
+						.build();
+				httpRequest.setURI(uri);*/
+				//HttpPut httpRequest = new HttpPut(URL);
+				//HttpDelete httpRequest = new HttpDelete(URL);
+				httpRequest.setEntity(new StringEntity(objectMapper.writeValueAsString(object)));
+				httpRequest.setHeader("Content-Type", "application/json; charset=UTF-8");
+				for(Header header : httpRequest.getAllHeaders()) {
+					log.info(header.getName() + "(Request): " + header.getValue());
+				}
+				httpClient.execute(httpRequest, new FutureCallback<HttpResponse>() {
+	                @Override
+	                public void completed(HttpResponse httpResponse) {
+	                    try {
+	                    	for(Header header : httpResponse.getAllHeaders()) {
+	        					log.info(header.getName() + "(Response): " + header.getValue());
+	        				}
+	        				log.info("HTTP Response code: " + httpResponse.getStatusLine().getStatusCode());
+	                    	HttpEntity entity = httpResponse.getEntity();
+	        				String responseString = EntityUtils.toString(entity);
+							log.info("Response: " + responseString);
+//							Read & update the response JSON parameter value into Object
+							Object object = objectMapper.readValue(responseString, Object.class);
+	                        futureObject.complete(object);
+	                    } catch (Exception e) {
+	                    	// Get the current stack trace element
+	    					StackTraceElement currentElement = Thread.currentThread().getStackTrace()[1];
+	    					// Find matching stack trace element from exception
+	    					for (StackTraceElement element : e.getStackTrace()) {
+	    						if (currentElement.getClassName().equals(element.getClassName())
+	    								&& currentElement.getMethodName().equals(element.getMethodName())) {
+	    							log.error("Error in {} at line {}: {} - {}",
+	    									element.getClassName(),
+	    									element.getLineNumber(),
+	    									e.getClass().getName(),
+	    									e.getMessage());
+	    							break;
+	    						}
+	    					}
+	                        futureObject.completeExceptionally(e);
+	                    }
+	                }
+
+	                @Override
+	                public void failed(Exception e) {
+	                	// Get the current stack trace element
+						StackTraceElement currentElement = Thread.currentThread().getStackTrace()[1];
+						// Find matching stack trace element from exception
+						for (StackTraceElement element : e.getStackTrace()) {
+							if (currentElement.getClassName().equals(element.getClassName())
+									&& currentElement.getMethodName().equals(element.getMethodName())) {
+								log.error("Error in {} at line {}: {} - {}",
+										element.getClassName(),
+										element.getLineNumber(),
+										e.getClass().getName(),
+										e.getMessage());
+								break;
+							}
+						}
+						futureObject.completeExceptionally(e);
+	                }
+
+	                @Override
+	                public void cancelled() {
+	                    log.warn("Async HTTP request cancelled");
+	                    futureObject.cancel(true);
+	                }
+	            });
+				// Option A: Throws checked exceptions
+				object = futureObject.get();  // blocks until completed
+				// Option B: Throws unchecked exceptions
+				object = futureObject.join();  // same as get(), but no checked exception
+				// Option C: You don't "get" it immediately, but instead you handle it via callback
+				futureObject.thenAccept(obj -> {
+				    // Do something with the result
+				    
+				});
+			}
+		} catch(SocketTimeoutException | ConnectTimeoutException e) {
+			// Get the current stack trace element
+			StackTraceElement currentElement = Thread.currentThread().getStackTrace()[1];
+			// Find matching stack trace element from exception
+			for (StackTraceElement element : e.getStackTrace()) {
+				if (currentElement.getClassName().equals(element.getClassName())
+						&& currentElement.getMethodName().equals(element.getMethodName())) {
+					log.error("Error in {} at line {}: {} - {}",
+							element.getClassName(),
+							element.getLineNumber(),
+							e.getClass().getName(),
+							e.getMessage());
+					break;
+				}
+			}
+		} catch(Exception e) {
+			// Get the current stack trace element
+			StackTraceElement currentElement = Thread.currentThread().getStackTrace()[1];
+			// Find matching stack trace element from exception
+			for (StackTraceElement element : e.getStackTrace()) {
+				if (currentElement.getClassName().equals(element.getClassName())
+						&& currentElement.getMethodName().equals(element.getMethodName())) {
+					log.error("Error in {} at line {}: {} - {}",
+							element.getClassName(),
+							element.getLineNumber(),
+							e.getClass().getName(),
+							e.getMessage());
+					break;
+				}
+			}
+		} finally {
+			try {
+				
+			}catch(Exception e) {}
+		}
+		return result;
+	}
 	
 	public String iso8583Api(Logger log, String logFolder) {
 		String result = "";
 		String ip = "";
 		int port = 0;
-		boolean wasNull = true;
 		Socket socket;
 		OutputStream outputStream;
 		InputStream inputStream;
