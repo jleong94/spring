72e4156db0d7cef837641c84b02b4b1c90e4f374
james.leong <james.leong@WS-LAPTOP-127.mpsb.net>
2025-08-26 09:09:45 +0800

    commit

--- src/main/java/com/pojo/Property.java
+++ src/main/java/com/pojo/Property.java
@@ -1,13 +1,15 @@
 package com.pojo;
 
 import java.util.List;
-
+import java.util.Properties;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import lombok.Getter;
 
@@ -31,6 +33,39 @@ public class Property {
 	//Email SMTP
 	@Value("${spring.mail.host}")
     private String spring_mail_host;
+
+	@Value("${spring.mail.port}")
+    private int spring_mail_port;
+
+	@Value("${spring.mail.username}")
+    private String spring_mail_username;
+
+	@Value("${spring.mail.password}")
+    private String spring_mail_password;
+
+	@Value("${spring.mail.properties.mail.smtp.auth}")
+    private String spring_mail_properties_mail_smtp_auth;
+
+	@Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String spring_mail_properties_mail_smtp_starttls_enable;
+
+	@Value("${spring.mail.protocol}")
+    private String spring_mail_protocol;
+	
+	@Bean
+    JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSenderImpl = new JavaMailSenderImpl();
+        javaMailSenderImpl.setHost(this.spring_mail_host);
+        javaMailSenderImpl.setPort(this.spring_mail_port);
+        javaMailSenderImpl.setUsername(this.spring_mail_username);
+        javaMailSenderImpl.setPassword(this.spring_mail_password);
+
+        Properties properties = javaMailSenderImpl.getJavaMailProperties();
+        properties.put("mail.smtp.auth", this.spring_mail_properties_mail_smtp_auth);
+        properties.put("mail.smtp.starttls.enable", this.spring_mail_properties_mail_smtp_starttls_enable);
+
+        return javaMailSenderImpl;
+    }
 	
 	//CORS
 	@Value("#{'${allowed.origins}'.split(',')}")
