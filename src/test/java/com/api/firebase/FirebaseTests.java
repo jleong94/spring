9175191c22e0d3c9719d51f5b6eab831c595f794
James Leong <jleong963@gmail.com>
2025-09-03 20:22:14 +0800

    commit

--- src/test/java/com/api/firebase/FirebaseTests.java
+++ src/test/java/com/api/firebase/FirebaseTests.java
@@ -16,11 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pojo.firebase.fcm.Message;
+import com.utilities.Tool;
 
 import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
@@ -36,6 +38,9 @@ public class FirebaseTests {
 	
 	@Autowired
     private ObjectMapper objectMapper;
+	
+	@MockitoBean
+    private Tool tool;
 	
 	@Test
 	@Order(1)
