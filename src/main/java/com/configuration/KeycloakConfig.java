9d107cbbfd47e15044ad38977e92ef9b091eab65
James Leong <james.leong@mpsb.net>
2025-11-24 14:59:40 +0800

    commit

--- src/main/java/com/configuration/KeycloakConfig.java
+++ src/main/java/com/configuration/KeycloakConfig.java
@@ -2,12 +2,17 @@ package com.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Configuration
 @ConfigurationProperties(prefix = "keycloak")
 @Data//Shortcut for @ToString, @EqualsAndHashCode, @Getter on all fields, and @Setter on all non_final fields, and @RequiredArgsConstructor
+@AllArgsConstructor//Generates a constructor with parameters for all fields (regardless of type or annotations)
+@NoArgsConstructor//Generates a constructor with no parameters
 @Builder(toBuilder = true)
 public class KeycloakConfig {
 
